ad408f9df7317429b84c7f60de1e74f27faf654a
veryrarestat <stmantaz@gmail.com>
2024-02-22 23:19:48 +0000

    Testing

--- script/skilltoggle.js
+++ script/skilltoggle.js
@@ -1,26 +1,35 @@
-const skillLabels = document.querySelectorAll('.languages > div');
+    // window.addEventListener('DOMContentLoaded', function() {
+    //     if (window.matchMedia('(max-width: 768px)').matches) {
+    //         const skillLabels = document.querySelectorAll('.languages > div');
 
-// Function to toggle visibility of proficiency level
-function toggleProficiency(proficiency) {
-    if (proficiency.style.visibility === 'visible') {
-        proficiency.style.visibility = 'hidden';
-    } else {
-        proficiency.style.visibility = 'visible';
-    }
-}
+    //         // Function to toggle visibility of proficiency level
+    //         function toggleProficiency(proficiency) {
+    //             if (proficiency.style.visibility === 'visible') {
+    //                 proficiency.style.visibility = 'hidden';
+    //             } else {
+    //                 proficiency.style.visibility = 'visible';
+    //             }
+    //         }
 
-// Add event listeners for click/tap to toggle proficiency level visibility
-skillLabels.forEach(skillLabel => {
-    skillLabel.addEventListener('click', function() {
-        const proficiency = skillLabel.querySelector('span');
-        toggleProficiency(proficiency);
-    });
+    //         // Add event listeners for click/tap to toggle proficiency level visibility
+    //         skillLabels.forEach(skillLabel => {
+    //             skillLabel.addEventListener('click', function() {
+    //                 const proficiency = skillLabel.querySelector('span');
+    //                 toggleProficiency(proficiency);
+    //             });
 
-    // Add touchstart event for touch devices
-    skillLabel.addEventListener('touchstart', function(event) {
-        // Prevent default touch behavior
-        event.preventDefault();
-        const proficiency = skillLabel.querySelector('span');
-        toggleProficiency(proficiency);
-    });
-});
+    //             // Add touchstart event for touch devices
+    //             skillLabel.addEventListener('touchstart', function(event) {
+    //                 // Prevent default touch behavior
+    //                 event.preventDefault();
+    //                 const proficiency = skillLabel.querySelector('span');
+    //                 toggleProficiency(proficiency);
+    //             });
+    //         });
+
+    //         // Prevent hiding proficiency levels initially
+    //         document.querySelectorAll('.languages span').forEach(proficiency => {
+    //             proficiency.style.visibility = 'visible';
+    //         });
+    //     }
+    // });
