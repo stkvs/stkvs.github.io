9a75bf86f3fe97f206df72d78abef1aea82a8726
veryrarestat <stmantaz@gmail.com>
2024-02-23 18:55:17 +0000

    Fix scroll

--- script/cover-toggle.js
+++ script/cover-toggle.js
@@ -1,5 +1,8 @@
+const body = document.querySelector('body')
 const homePage = document.querySelector('.cover');
 const logo = document.querySelector('.logo-blue');
+
+body.style.overflowY = "hidden";
 
 homePage.style.opacity = 1;
 homePage.style.transition = "opacity 0.5s ease"; // Apply transition effect to opacity
@@ -7,6 +10,7 @@ homePage.style.transition = "opacity 0.5s ease"; // Apply transition effect to o
 logo.addEventListener('click', () => {
     console.log("Clicked");
     homePage.style.opacity = 0;
+    body.style.overflowY = "scroll";
 
     // Wait for the transition to complete before hiding the element
     setTimeout(() => {
