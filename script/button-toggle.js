86733e314229a94fdac9ab3b2d0f0d6fd15ab35d
veryrarestat <stmantaz@gmail.com>
2024-03-18 14:00:20 +0000

    Adjusted cover button

--- script/button-toggle.js
+++ script/button-toggle.js
@@ -1,23 +1,17 @@
 import { body, homePage } from './cover-toggle.js';
 
 const button = document.querySelector('.back-to-cover');
+const bg = document.querySelector('.bg1');
 
 button.addEventListener('click', () => {
     console.log("Clicked");
-
-    const mainPage = document.querySelector('#main-page');
-
-    if (mainPage) {
-        window.scrollTo({
-            top: mainPage.offsetTop, 
-            behavior: 'smooth'
-        });
-    }
 
     body.style.overflowY = "hidden";
     homePage.style.display = "block";
 
     homePage.style.transition = "opacity 0.5s ease";
+    bg.style.display = "block";
+
 
     setTimeout(() => {
         homePage.style.opacity = 1;
