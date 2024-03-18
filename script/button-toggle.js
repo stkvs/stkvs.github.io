572ccbb674a96127986f24a7297da379c712d016
veryrarestat <stmantaz@gmail.com>
2024-03-18 15:23:27 +0000

    Adjusted button toggle
    Checking whether the window resized to then initialise the Particle background

--- script/button-toggle.js
+++ script/button-toggle.js
@@ -1,22 +1,31 @@
 import { body, homePage } from './cover-toggle.js';
 
 const button = document.querySelector('.back-to-cover');
-const bg = document.querySelector('.bg1');
-const bgParticles = document.querySelector('#bg-particles-js')
+const bgParticles = document.querySelector('#bg-particles-js');
 
 button.addEventListener('click', () => {
     console.log("Clicked");
 
     body.style.overflowY = "hidden";
     homePage.style.display = "block";
-
-    homePage.style.transition = "opacity 0.5s ease";
+    
+    homePage.style.transition = "opacity .5s ease";
+    bgParticles.style.transition = "opacity .5s ease";
 
     setTimeout(() => {
         homePage.style.opacity = 1;
-        // bg.style.height = "100svh";
-        // bg.style.position = "absolute";
-        // bgParticles.style.display = "block";
-        // bg.style.display = "block";
+        bgParticles.style.opacity = 1;
     }, 100);
+});
+
+// Listen for resize events
+window.addEventListener('resize', () => {
+    // Check if URL bar is visible
+    if (window.innerHeight < window.outerHeight) {
+        // URL bar is visible, hide bgParticles
+        bgParticles.style.opacity = 0;
+    } else {
+        // URL bar is not visible, show bgParticles
+        bgParticles.style.opacity = 1;
+    }
 });
