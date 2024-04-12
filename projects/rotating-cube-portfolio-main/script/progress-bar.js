5197965d3a79511955d1cc6147a3c62b2562c61c
veryrarestat <stmantaz@gmail.com>
2024-04-13 00:29:15 +0100

    Changing redirect to a seperate repo

--- projects/rotating-cube-portfolio-main/script/progress-bar.js
+++
@@ -1,9 +0,0 @@
-function updateProgressBar(){
-    const {scrollTop, scrollHeight} = document.documentElement;
-    const scrollPercent = scrollTop / (scrollHeight - window.innerHeight) * 100 + '%';
-
-    document.querySelector('#progress-bar').style.setProperty('--progress', scrollPercent);
-    // console.log(scrollPercent)
-}
-
-document.addEventListener('scroll', updateProgressBar);
