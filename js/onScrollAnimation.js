ef74912f0c9f70eb0092758310742be958369a94
veryrarestat <stmantaz@gmail.com>
2024-04-29 23:37:07 +0100

    V.1.2
    Removed fade in animation from mobile, as it has bugs

--- js/onScrollAnimation.js
+++ js/onScrollAnimation.js
@@ -1,18 +1,20 @@
 // JavaScript
 window.addEventListener('scroll', function() {
-    const elements = document.querySelectorAll('.page'); // replace .element with your element selector
-    const window_height = window.innerHeight;
-    const scroll_y = window.scrollY;
-  
-    elements.forEach(function(element) {
-      const element_position = element.getBoundingClientRect().top + scroll_y;
-      const element_height = element.offsetHeight;
-  
-      // Check if the element is in the viewport and not the element with ID 'home'
-      if (scroll_y > element_position - window_height && scroll_y < element_position + element_height && element.id !== 'home') {
-        element.classList.add('animate');
-      } else {
-        element.classList.remove('animate');
-      }
-    });
+    if (window.innerWidth > 768 && !('ontouchstart' in window || navigator.maxTouchPoints)) {
+        const elements = document.querySelectorAll('.page'); // replace .element with your element selector
+        const window_height = window.innerHeight;
+        const scroll_y = window.scrollY;
+
+        elements.forEach(function(element) {
+            const element_position = element.getBoundingClientRect().top + scroll_y;
+            const element_height = element.offsetHeight;
+
+            // Check if the element is in the viewport and not the element with ID 'home'
+            if (scroll_y > element_position - window_height && scroll_y < element_position + element_height && element.id !== 'home') {
+                element.classList.add('animate');
+            } else {
+                element.classList.remove('animate');
+            }
+        });
+    }
 });
