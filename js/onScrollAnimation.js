846e5ddfd4177ecc6ba19406e811f75e67192c98
veryrarestat <stmantaz@gmail.com>
2024-04-29 23:23:42 +0100

    V1.1
    Added: GA4, on Scroll animations and landscape mobile responsiveness

---
+++ js/onScrollAnimation.js
@@ -0,0 +1,18 @@
+// JavaScript
+window.addEventListener('scroll', function() {
+    const elements = document.querySelectorAll('.page'); // replace .element with your element selector
+    const window_height = window.innerHeight;
+    const scroll_y = window.scrollY;
+  
+    elements.forEach(function(element) {
+      const element_position = element.getBoundingClientRect().top + scroll_y;
+      const element_height = element.offsetHeight;
+  
+      // Check if the element is in the viewport and not the element with ID 'home'
+      if (scroll_y > element_position - window_height && scroll_y < element_position + element_height && element.id !== 'home') {
+        element.classList.add('animate');
+      } else {
+        element.classList.remove('animate');
+      }
+    });
+});
