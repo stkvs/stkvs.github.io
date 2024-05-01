7d66cdf706c4f69a9a01609ed639d77df5fcce16
veryrarestat <stmantaz@gmail.com>
2024-05-01 16:47:33 +0100

    V1.6.6
    Removed: Comments

--- js/onScrollAnimation.js
+++ js/onScrollAnimation.js
@@ -1,7 +1,6 @@
-// JavaScript
 window.addEventListener('scroll', function() {
     if (window.innerWidth > 768 && !('ontouchstart' in window || navigator.maxTouchPoints)) {
-        const elements = document.querySelectorAll('.page'); // replace .element with your element selector
+        const elements = document.querySelectorAll('.page');
         const window_height = window.innerHeight;
         const scroll_y = window.scrollY;
 
@@ -9,7 +8,6 @@ window.addEventListener('scroll', function() {
             const element_position = element.getBoundingClientRect().top + scroll_y;
             const element_height = element.offsetHeight;
 
-            // Check if the element is in the viewport and not the element with ID 'home'
             if (scroll_y > element_position - window_height && scroll_y < element_position + element_height && element.id !== 'home') {
                 element.classList.add('animate');
             } else {
